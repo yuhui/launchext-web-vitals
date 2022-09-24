46c823bfe2dba9e127ea08b312380b7e5347e91b
Yuhui <yuhui@users.noreply.github.com>
2022-09-24 17:47:08 +0800

    chore: update copyright message

--- spec/lib/events/cumulativeLayoutShift.spec.js
+++ spec/lib/events/cumulativeLayoutShift.spec.js
@@ -1,5 +1,5 @@
 /**
- * Copyright 2021 Yuhui. All rights reserved.
+ * Copyright 2021-2022 Yuhui. All rights reserved.
  *
  * Licensed under the GNU General Public License, Version 3.0 (the "License");
  * you may not use this file except in compliance with the License.
